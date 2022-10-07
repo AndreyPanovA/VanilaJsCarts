d0d36b0a9a4c263c638618a899d7957dd5cdee0c
AndreyPanovA <a.andrey.panov@gmail.com>
2022-10-07 18:17:53 +0300

    drawLines

--- index1.js
+++ index1.js
@@ -1,48 +1,66 @@
 const HEIGHT = 400
-const WIDTH = 600
+const WIDTH = 800
 const DPI_WIDTH = WIDTH * 2
 const DPI_HEIGHT = HEIGHT * 2
 const ROWS_COUNT = 5
 const PADDING = 40
-const STEP = Math.floor(DPI_HEIGHT / ROWS_COUNT)
 const VIEW_HEIGHT = DPI_HEIGHT - PADDING * 2
-const VIEW_WIDTH = DPI_WIDTH - PADDING * 2
+const VIEW_WIDTH = DPI_WIDTH - PADDING
+const STEP = VIEW_HEIGHT / ROWS_COUNT
 const FONT = "22px serif"
-
-function calcY(y) {
-    return VIEW_HEIGHT - y
-}
-
-function drawGrid(ctx, yRatio) {
-    ctx.beginPath()
-    ctx.moveTo(0, 0)
-    ctx.lineTo(0, VIEW_HEIGHT)
-    ctx.stroke()
-    ctx.closePath()
-    ctx.beginPath()
-    for (let i = 0; i < ROWS_COUNT; i++) {
-        let y = calcY(i * STEP)
-        let x = 0
-        ctx.moveTo(x, y)
-        ctx.lineTo(DPI_WIDTH, y)
-        ctx.strokeStyle = "#bbb"
-        ctx.font = FONT;
-        ctx.fillText(calcY(y), x + PADDING / 2, y - PADDING / 4)
+const WIDTH_SUBSCRIBE_PADDING = 2
+const {minY, maxY, yRatio, xRatio} = computeData(dataPreparation())
+const textStep = (maxY - minY) / ROWS_COUNT
+function dataPreparation() {
+    const {columns, colors, types} = getChartData()
+    return {
+        x: columns[0].filter((el, idx) => idx > 0),
+        xy: columns.map((el) => {
+            const name = el?.[0]
+            const isLine = types[name] == 'line'
+            if (isLine) {
+                return el.map((item, idx) => [idx, item]).filter((el, idx) => idx > 0)
+            }
+        }).filter(Boolean),
+        colors: Object.values(colors),
     }
-    ctx.stroke()
-    ctx.closePath()
 }
 
-function draw(ctx, data, yRatio ) {
-    ctx.beginPath()
-    ctx.lineWidth = 4
+
+function gridLineStyle(ctx) {
+    ctx.strokeStyle = "#bbb"
+    ctx.stroke()
+}
+
+function chartLineStyle(ctx, color = "#ff0000", lineWidth = 4) {
+    ctx.strokeStyle = color
     ctx.lineCap = "round"
-    ctx.strokeStyle = "#ff0000"
-    for (let [x, y] of data) {
-        console.log(yRatio)
-        ctx.lineTo(x + PADDING, calcY(y*yRatio))
-    }
+    ctx.lineWidth = lineWidth
     ctx.stroke()
+}
+
+function drawGrid(ctx) {
+    ctx.beginPath()
+    ctx.font = FONT;
+    for (let i = 1; i <= ROWS_COUNT; i++) {
+        let y = i * STEP
+        const text = Math.round(maxY - textStep * i).toString()
+        ctx.moveTo(0, y + PADDING)
+        ctx.lineTo(DPI_WIDTH, y + PADDING)
+        ctx.fillText(text, 0, y + PADDING)
+    }
+    gridLineStyle(ctx)
+    ctx.closePath()
+}
+
+function draw(ctx, {xy, colors}) {
+    ctx.beginPath()
+    for (let data in xy) {
+        ctx.beginPath()
+        for (let [x, y] of xy[data]) ctx.lineTo(x * xRatio, DPI_HEIGHT - PADDING - y * yRatio)
+        chartLineStyle(ctx, colors[data])
+        ctx.closePath()
+    }
     ctx.closePath()
 }
 
@@ -52,33 +70,29 @@ const chart = (canvas, data) => {
     canvas.height = DPI_HEIGHT
     canvas.style.width = WIDTH + "px"
     canvas.style.height = HEIGHT + "px"
-    const {minY, maxY} = computeData(data)
-    const yRatio = VIEW_HEIGHT/(maxY-minY)
-    drawGrid(ctx, yRatio)
-    draw(ctx, data, yRatio)
+    drawGrid(ctx)
+    draw(ctx, data)
 }
 
-function computeData(data) {
+function computeData({x, xy}) {
     let minX = Infinity
     let maxX = -Infinity
     let minY = Infinity
     let maxY = -Infinity
-    for (let [x, y] of data) {
-        if (minY > y) minY = y
-        if (maxY < y) maxY = y
-        if (minX > x) minX = x
-        if (maxX < x) maxX = x
+    for (let data in xy) {
+        for (let [x, y] of xy[data]) {
+            if (minY > y) minY = y
+            if (maxY < y) maxY = y
+            if (minX > x) minX = x
+            if (maxX < x) maxX = x
+        }
     }
-    return {minX, minY, maxX, maxY}
+    const yRatio = VIEW_HEIGHT / (maxY - minY)
+    const xRatio = VIEW_WIDTH / x.length + WIDTH_SUBSCRIBE_PADDING
+    return {minX, minY, maxX, maxY, yRatio, xRatio}
 }
 
 
-chart(document.getElementById("chart"), [
-    [0, 0],
-    [300, 480],
-    [500, 160],
-    [700, 5180],
-    [800, 1200],
-    [1000, 100],
-    [1200, 500]
-])
+chart(document.getElementById("chart"), dataPreparation())
+
+
